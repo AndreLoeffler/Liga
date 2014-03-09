ffa7bc46c127b2449aa0e9070f5f1c3c2d340b75
Andre Löffler <github@andre-loeffler.net>
2014-03-09 11:05:25 +0100

    armee enbedded in base template, security token as service

--- src/Binaerpiloten/LigaBundle/Controller/DefaultController.php
+++ src/Binaerpiloten/LigaBundle/Controller/DefaultController.php
@@ -8,30 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	$csrfToken = $this->container->has('form.csrf_provider')
-    	? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
-    	: null;
-    	
-//     	if( $this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ) {
-//     		$armeen = $this->getArmeenAction();
-//     	} else {
-//     		$armeen = "";
-//     	}
-    	
       return $this->render('BinaerpilotenLigaBundle::base.html.twig', 
-      		array('csrf_token' => $csrfToken/*, 'armeen' => $armeen*/));
+      		array());
     }
-    
-//     public function getArmeenAction() {
-//     	$em = $em = $this->getDoctrine()->getManager();
-//       $armeen = array();
-
-//       $qarmeen = $em->createQuery("SELECT r " .
-//       		"FROM Binaerpiloten\LigaBundle\Entity\Armee r " .
-//       		"WHERE r.user = ".$this->getUser()->getId() );
-      
-//       $armeen = $qarmeen->getResult();
-      
-//       return $armeen;
-//     }
 }
