ce651917eeb3673b4c35d869e2b94d3f62aed74b
Andre Löffler <github@andre-loeffler.net>
2014-03-18 18:49:17 +0100

    spiel-filter now working for year and armee

--- src/Binaerpiloten/LigaBundle/Controller/FilterController.php
+++ src/Binaerpiloten/LigaBundle/Controller/FilterController.php
@@ -19,23 +19,6 @@ class FilterController extends Controller
 {
 
     /**
-     * Lists all Filter entities.
-     *
-     * @Route("/", name="filter")
-     * @Method("GET")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entities = $em->getRepository('BinaerpilotenLigaBundle:Filter')->findAll();
-
-        return array(
-            'entities' => $entities,
-        );
-    }
-    /**
      * Creates a new Filter entity.
      *
      * @Route("/", name="filter_create")
@@ -49,11 +32,8 @@ class FilterController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
 
-            return $this->redirect($this->generateUrl('filter_show', array('id' => $entity->getId())));
+            return $this->displayResults($entity);
         }
 
         return array(
@@ -99,149 +79,39 @@ class FilterController extends Controller
         );
     }
 
+    /* ================== Helpers go here ==================== */
+    
     /**
-     * Finds and displays a Filter entity.
-     *
-     * @Route("/{id}", name="filter_show")
-     * @Method("GET")
-     * @Template()
+     * Displays list of matches filtered by Filter-object.
      */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('BinaerpilotenLigaBundle:Filter')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Filter entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-     * Displays a form to edit an existing Filter entity.
-     *
-     * @Route("/{id}/edit", name="filter_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('BinaerpilotenLigaBundle:Filter')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Filter entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Filter entity.
-    *
-    * @param Filter $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Filter $entity)
-    {
-        $form = $this->createForm(new FilterType(), $entity, array(
-            'action' => $this->generateUrl('filter_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Filter entity.
-     *
-     * @Route("/{id}", name="filter_update")
-     * @Method("PUT")
-     * @Template("BinaerpilotenLigaBundle:Filter:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('BinaerpilotenLigaBundle:Filter')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Filter entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('filter_edit', array('id' => $id)));
-        }
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Filter entity.
-     *
-     * @Route("/{id}", name="filter_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('BinaerpilotenLigaBundle:Filter')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Filter entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('filter'));
-    }
-
-    /**
-     * Creates a form to delete a Filter entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('filter_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
+    private function displayResults(Filter $filter) {
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	
+    	$entities = $em->getRepository('BinaerpilotenLigaBundle:Spiel')->findAll();
+    	
+    	if ($filter->getYear() != null) {
+    		$year = $filter->getYear();
+    		foreach ($entities as $k=>$e) {
+    			if($e->getDatum()->format('Y') != $year) {
+    				unset($entities[$k]);
+    			}
+    		}
+    	}
+    	
+    	if ($filter->getVolk() != null) {
+    		$volk = $filter->getVolk();
+    		foreach ($entities as $k=>$e) {
+    			if($e->getYouarmee()->getVolk() != $volk && $e->getEnemyarmee()->getVolk() != $volk) {
+    				unset($entities[$k]);
+    			}
+    		}
+    	}
+    	
+    	return $this->render(
+    			'BinaerpilotenLigaBundle:Spiel:index.html.twig',
+    			array('entities' => $entities)
+    	);
+    	
     }
 }
